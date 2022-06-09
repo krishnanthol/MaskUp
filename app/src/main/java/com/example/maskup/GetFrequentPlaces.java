5080b9482a40fe01518d252277d07d048f1c1755
krishnanthol <inbantholkappian@gmail.com>
2022-06-09 14:22:17 -0400

    adsgfhidsfglkfdsg

--- app/src/main/java/com/example/maskup/GetFrequentPlaces.java
+++ app/src/main/java/com/example/maskup/GetFrequentPlaces.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 
 import org.json.JSONException;
 
@@ -16,7 +18,7 @@ public class GetFrequentPlaces extends AsyncTask<Void, Void, Void>
     {
         try
         {
-            MainActivity.addresses = MainActivity.geocoder.getFromLocationName("South Brunswick High School",5);
+            MainActivity.addresses = MainActivity.geocoder.getFromLocationName("57 Potomac Road",5);
             Log.d("addresses",MainActivity.addresses.toString());
 
             MainActivity.customAdapter = new CustomAdapter(MainActivity.context,R.layout.adapter_custom,MainActivity.addresses);
@@ -26,6 +28,15 @@ public class GetFrequentPlaces extends AsyncTask<Void, Void, Void>
                 public void run()
                 {
                     MainActivity.listView.setAdapter(MainActivity.customAdapter);
+
+                    MainActivity.listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+                    {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+                        {
+
+                        }
+                    });
                 }
             });
         }
